a2fd875d1515532f437af91226e59e51397b742a
kelvinadeu <kelvinadeu25@gmail.com>
2019-03-23 12:48:43 +0300

    added models

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
-    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
     first_name = models.CharField(max_length = 30,null=True)
     last_name = models.CharField(max_length = 30, null=True)
+    bio = models.CharField(max_length=100)
+    Profile_pic = models.ImageField(upload_to='profile/')
