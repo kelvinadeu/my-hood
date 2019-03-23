ff3e8a77777e4e36fadac3b41367f08d03c98c36
kelvinadeu <kelvinadeu25@gmail.com>
2019-03-23 12:26:06 +0300

    added class model profile

--- hood/settings.py
+++ hood/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'neighbourhood',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -75,11 +77,12 @@ WSGI_APPLICATION = 'hood.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'adeu',
+        'USER': 'moringaschool',
+    'PASSWORD':'1234',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -105,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Profile(models.Model):
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length = 30,null=True)
+    last_name = models.CharField(max_length = 30, null=True)
