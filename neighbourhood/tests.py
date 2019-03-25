899570c88d0940f3bf3162e50dc941593bfefc00
kelvinadeu <kelvinadeu25@gmail.com>
2019-03-25 21:15:24 +0300

    added readme

--- neighbourhood/tests.py
+++ neighbourhood/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import *
+
 
 # Create your tests here.
+class UserProfileTestClass(TestCase):
+    def setUp(self):
+        self.new_user = User.objects.create_user(username='user',password='user-password')
+        self.new_profile = UserProfile(id=1,first_name='Firstname',last_name='Lastname',user=self.new_user,location='Test Location')
+        
