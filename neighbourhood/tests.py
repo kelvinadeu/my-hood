65078fdf719b3b76057741158dd0835544d24fd5
kelvinadeu <kelvinadeu25@gmail.com>
2019-03-25 21:16:17 +0300

    added tests

--- neighbourhood/tests.py
+++ neighbourhood/tests.py
@@ -8,4 +8,6 @@ class UserProfileTestClass(TestCase):
     def setUp(self):
         self.new_user = User.objects.create_user(username='user',password='user-password')
         self.new_profile = UserProfile(id=1,first_name='Firstname',last_name='Lastname',user=self.new_user,location='Test Location')
-        
+
+def test_instance(self):
+        self.assertTrue(isinstance(self.new_profile,UserProfile))
