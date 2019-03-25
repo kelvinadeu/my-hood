9682fcfcedcf26637e5c28093e2db6363f061316
kelvinadeu <kelvinadeu25@gmail.com>
2019-03-25 14:17:24 +0300

    added changes in profile class

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -1,9 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 class Profile(models.Model):
-    user_id = models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
+    user = models.CharField(max_length = 30,null=True)
     first_name = models.CharField(max_length = 30,null=True)
     last_name = models.CharField(max_length = 30, null=True)
-    bio = models.CharField(max_length=100)
-    Profile_pic = models.ImageField(upload_to='profile/')
+    bio = models.TextField(blank=True)
+    # Profile_pic = models.ImageField(upload_to='profile/')
+    # hood = models.ForeignKey('Hood', blank=True, null=True)
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def get_profiles(cls):
+        profiles = cls.objects.all()
+        return profiles
+
+@receiver(post_save,sender=User)
+def Create_user_profile(sender,instance,created, **kwargs):
+    if created:
+        profile.objects.created(user=instance)
+
+@receiver(post_save,sender=User)
+def save_user_profile(sender,instance, **kwargs):
+    insatnce.profile.save()
